98ddeece9af69bb95686e69fc1b5f75f6496b54c
小松 聖矢 <skomatsu@skomatsu-MBP.local>
2017-12-18 13:05:47 +0900

    05追加

--- chapter1/05.py
+++ chapter1/05.py
@@ -1,9 +1,26 @@
 # chapter1 - 04
-n = input("n ? ")
-n = int(n)
+def main():
+    n = input("n ? ")
+    w = int("string ? ")
+
+    cn = character_n_gram(w, n)
+    wn = word_n_gram(w, n)
+
+    print("文字N-gram")
+    print(cn)
+    print("単語N-gram")
+    print(wn)
+
 
 # 単語n-gram
-
+def word_n_gram(w, n):
+    n = int(n)
 
 
 # 文字n-gram
+def character_n_gram(w, n):
+    n = int(n)
+
+
+if __name__ == "__main__":
+    main()
